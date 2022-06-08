b80a0fa0e3ce724e798944ed342deca715c00465
H N Lakshmi Narasimha <82299708+hnln9063@users.noreply.github.com>
2022-06-08 22:12:17 +0530

    NQueens Problem to print all Solutions

---
+++ DAA/NQueensAllSolutions.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class NQueensAllSolutions {
+	static int count=0;
+	public static boolean isSafe(char[][] mat, int r, int c) {
+		for (int i = 0; i < r; i++) {
+			if (mat[i][c] == 'Q') {
+				return false;
+			}
+		}
+
+		for (int i = r, j = c; i >= 0 && j >= 0; i--, j--) {
+			if (mat[i][j] == 'Q') {
+				return false;
+			}
+		}
+
+		for (int i = r, j = c; i >= 0 && j < mat.length; i--, j++) {
+			if (mat[i][j] == 'Q') {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static void printSolution(char[][] mat) {
+		for (char[] chars: mat) {
+			System.out.println(Arrays.toString(chars).replaceAll(",", ""));
+		}
+		System.out.println();
+
+		count++;
+	}
+
+	private static void nQueen(char[][] mat, int r) {
+		if (r == mat.length) {
+			printSolution(mat);
+			return;
+		}
+		for (int i = 0; i < mat.length; i++) {
+			if (isSafe(mat, r, i)) {
+				mat[r][i] = 'Q'; 
+				nQueen(mat, r + 1);
+				mat[r][i] = '–';
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int N;
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextInt();
+		char[][] mat = new char[N][N];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(mat[i], '–');
+		}
+		nQueen(mat, 0);
+		System.out.println(count);
+	}
+}
